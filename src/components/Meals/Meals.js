0a6dd22ca9d165887b025d942eb47088d4b3f298
Kamalesh <kamalesh5112000@pec.edu.com>
2024-02-14 21:13:17 +0530

    Meals Summery commit

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MealsSummery from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
+
+const Meals=()=>{
+    return <React.Fragment>
+        <MealsSummery/>
+        <AvailableMeals/>
+    </React.Fragment>
+
+}
+
+export default Meals;
