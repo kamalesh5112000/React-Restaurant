c3dba0a7d6cf4ecf18142325b618b63496571d47
Kamalesh <kamalesh5112000@pec.edu.com>
2024-02-15 21:16:56 +0530

    Cart show  commit

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,20 @@
+import React,{useContext} from 'react';
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+    const cartCtx=useContext(CartContext)
+    const numberOfCartItem=cartCtx.items.reduce((curNumer,item)=>{
+        return curNumer+item.amount;
+    },0);
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>0</span>
+      <span className={classes.badge}>{numberOfCartItem}</span>
     </button>
   );
 };
